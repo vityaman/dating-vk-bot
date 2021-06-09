c00dabee7c47d4c3514f4e608fe0c0237ee3997b
Victor <victorman.cosmos555@gmail.com>
2021-06-09 15:05:55 +0300

    Delete tabs in strings.py

--- resources/strings.py
+++ resources/strings.py
@@ -2,88 +2,84 @@ import config
 import resources.emojis as emoji
 import resources.languages as lang
 
-
 if config.LANGUAGE == lang.ru:
-    begin = \
-        f"""Начать"""
+    begin = "Начать"
     begin_hello = \
-        f"""Добро пожаловать!
-    Для начала зарегаем тебя.
-    Вот настроечки! Заполни данные:"""
+        ("Добро пожаловать!\n"
+         "Для начала зарегаем тебя.\n"
+         "Вот настроечки! Заполни данные:")
     begin_guide = \
-        f"""О, сладкий!
-    Ты тут впервые?
-    Нажми "Начать", чтобы продолжить :)"""
+        ("О, сладкий!\n"
+         "Ты тут впервые?\n"
+         "Нажми \"Начать\", чтобы продолжить :)")
     main_menu_guide = \
-        f"""Ты в главном меню.
-    Доступные функции:
-    {emoji.search} Поиск (Начать искать друзей)
-    {emoji.fax} Чат (Переписываться с рандомами)
-    {emoji.settings} Настройки (Изменить свои данные)"""
+        (f"Ты в главном меню.\n"
+         f"Доступные функции:\n"
+         f"{emoji.search} Поиск (Начать искать друзей)\n"
+         f"{emoji.fax} Чат (Переписываться с рандомами)\n"
+         f"{emoji.settings} Настройки (Изменить свои данные)")
     form_heading = \
-        f"""| Ваш аккаунт |
-            
-            """
+        (f"| Ваш аккаунт |\n\n")
     main_menu_preparing_suggestions = \
-        f"""Ищем вам друзей..."""
+        f"Ищем вам друзей..."
     main_menu_found = \
-        f"""Готово! Найдено людей:"""
+        f"Готово! Найдено людей:"
     main_menu_welcome_chat = \
-        f"""Добро пожаловать в чат!
-    Здесь ты можешь пообщаться.
-    Нажми {emoji.search} чтобы найти диалог!"""
+        (f"Добро пожаловать в чат!\n"
+         f"Здесь ты можешь пообщаться.\n"
+         f"Нажми {emoji.search} чтобы найти диалог!")
     searching_friend_exit = \
-        f"""На сегодня все, больше пользователей нет!
-    Добро пожаловать обратно в главное меню!"""
+        (f"На сегодня все, больше пользователей нет!\n"
+         f"Добро пожаловать обратно в главное меню!")
     searching_friend_look_next = \
-        f"""Опа, смотри кого нашел:"""
+        f"Опа, смотри кого нашел:"
     searching_friend_guide = \
-        f"""Ты в разделе поиска друзей!
-    Доступные функции:
-    {emoji.like} Лайк (Лайкнуть человека)
-    {emoji.dislike} Дальше (Получить еще одного)
-    {emoji.report} Пожаловаться (Отправить жалобу)
-    {emoji.back} Назад (Вернуться в главное мен)"""
+        (f"Ты в разделе поиска друзей!\n"
+         f"Доступные функции:\n"
+         f"{emoji.like} Лайк (Лайкнуть человека)\n"
+         f"{emoji.dislike} Дальше (Получить еще одного)\n"
+         f"{emoji.report} Пожаловаться (Отправить жалобу)\n"
+         f"{emoji.back} Назад (Вернуться в главное мен)")
     searching_friend_match_1 = \
-        f"""Поздравляю!
-    У вас взаимная лубов.
-    Вот ссылочка на вконтактик:"""
+        (f"Поздравляю!\n"
+         f"У вас взаимная лубов.\n"
+         f"Вот ссылочка на вконтактик:")
     searching_friend_match_2 = \
-        f"""Тили-тили тесто! поздравляю, боец!
-    Наконец-то ты хоть кому-то приглянулся:"""
+        (f"Тили-тили тесто! поздравляю, боец!\n"
+         f"Наконец-то ты хоть кому-то приглянулся:")
     searching_friend_match_motivation_1 = \
-        f"""Приятного времяпрепровождения!"""
+        f"Приятного времяпрепровождения!"
     searching_friend_match_motivation_2 = \
-        f"""Не стесняйся!"""
+        f"Не стесняйся!"
     report_was_sent = \
-        f"""Спасибки! Жалоба отправлена админу!"""
+        f"Спасибки! Жалоба отправлена админу!"
     chat_queue = \
-        f"""Ты в очереди...
-        подожди немножко..."""
+        (f"Ты в очереди...\n"
+         f"подожди немножко...")
     chat_found = \
-        f"""Нашелся собеседник:"""
+        f"Нашелся собеседник:"
     chat_talk = \
-        f"""Базарьте!"""
+        f"Базарьте!"
     chat_exit = \
-        f"""Ясно. Пока!
-        Увидимся"""
+        (f"Ясно. Пока!\n"
+         f"Увидимся")
     chat_guide = \
-        f"""Ты в разделе чата!
-    Доступные функции:
-    {emoji.search} Поиск (Искать беседу)
-    {emoji.report} Пожаловаться (Отправить жалобу)
-    {emoji.back} Назад (Вернуться в главное меню)"""
+        (f"Ты в разделе чата!\n"
+         f"Доступные функции:\n"
+         f"{emoji.search} Поиск (Искать беседу)\n"
+         f"{emoji.report} Пожаловаться (Отправить жалобу)\n"
+         f"{emoji.back} Назад (Вернуться в главное меню)")
     chatting_exit = \
-        f"""Добазарились спокойно, без лишних слов.
-    Эх, хорошо побалакали... {emoji.sad}"""
+        (f"Добазарились спокойно, без лишних слов.\n"
+         f"Эх, хорошо побалакали... {emoji.sad}")
     settings_guide = \
-        f"""Ты в настройках, братанчик.
-    Доступные функции:
-    {emoji.smile} Имя (Ввести имя)
-    {emoji.glasses} Возраст (Указать возраст)
-    {emoji.talk} О себе (Написать о себе)
-    {emoji.camera} Фото (Установить аву)
-    {emoji.save} Сохранить (Сохранить изменения)"""
+        (f"Ты в настройках, братанчик.\n"
+         f"Доступные функции:\n"
+         f"{emoji.smile} Имя (Ввести имя)\n"
+         f"{emoji.glasses} Возраст (Указать возраст)\n"
+         f"{emoji.talk} О себе (Написать о себе)\n"
+         f"{emoji.camera} Фото (Установить аву)\n"
+         f"{emoji.save} Сохранить (Сохранить изменения)")
     name = "Имя"
     age = "Возраст"
     about = "О себе"
@@ -92,121 +88,120 @@ if config.LANGUAGE == lang.ru:
     save = "Сохранить"
     cancel = "Отмена"
     settings_name = \
-        f"""Как звать-то тебя, милаш?"""
+        f"Как звать-то тебя, милаш?"
     settings_age = \
-        f"""И сколько же тебе лет?
-    Чур не обманывать!"""
+        (f"И сколько же тебе лет?\n"
+         f"Чур не обманывать!")
     settings_about = \
-        f"""Ну давай, расскажи о себе!
-    Мне, оч интересно, паравда..."""
+        (f"Ну давай, расскажи о себе!\n"
+         f"Мне, оч интересно, паравда...")
     settings_interests = \
-        f"""Чем ты интересуешься?
-    Чтобы улучшить качество подбора друзей,
-    Просто введи номера тэгов:
-        """
+        (f"Чем ты интересуешься?\n"
+         f"Чтобы улучшить качество подбора друзей,\n"
+         f"Просто введи номера тэгов:\n\n")
     settings_photo = \
-        f"""Все хотят тебя... увидеть!
-    Да я знаю, что ты милашечка.
-    Send photo pls!!"""
+        (f"Все хотят тебя... увидеть!\n"
+         f"Да я знаю, что ты милашечка.\n"
+         f"Send photo pls!!")
     accepted = "Принято!"
     settings_fill_all = \
-        f"""Все поля должны быть заполнены"""
+        f"Все поля должны быть заполнены"
     settings_return = \
-        f"""Вернул тебя назад."""
+        f"Вернул тебя назад."
     settings_invalid_input = \
-        f"""Это введеные данные инвалид или ты?"""
+        f"Это введеные данные инвалид или ты?"
     settings_ok_name = \
-        f"""Приятно познакомиться!"""
+        f"Приятно познакомиться!"
     settings_ok_age = \
-        f"""О, круть! В твоем возрасте самое время искать себе лубов!
-    Без этого никак..."""
+        (f"О, круть! В твоем возрасте самое время искать себе лубов!\n"
+         f"Без этого никак...")
     settings_ok_about = \
-        f"""Ну, ты крутой чел, да."""
+        f"Ну, ты крутой чел, да."
     settings_ok_interests = \
-        f"""Интересы приняты!"""
+        f"Интересы приняты!"
     settings_ok_photo = \
-        f"""Ты... т... т... такая красивая..., 
-    ой, ладно... я пошел... {emoji.shyhands}"""
+        (f"Ты... т... т... такая красивая..., \n"
+         f"ой, ладно... я пошел... {emoji.shyhands}")
 
 elif config.LANGUAGE == lang.eng:
     begin = \
-        f"""Begin"""
+        f"Begin"
     begin_hello = \
-        f"""Welcome!
-    We'll register you first.
-    Here are the settings. Fill the from:"""
+        (f"Welcome!\n"
+         f"We'll register you first.\n"
+         f"Here are the settings. Fill the from:")
     begin_guide = \
-        f"""Oh, hello, honey!
-    Are you for the first time here?
-    Press "Begin" to continue :)"""
+        (f"Oh, hello, honey!\n"
+         f"Are you for the first time here?\n"
+         f"Press \"Begin\" to continue :)")
     main_menu_guide = \
-        f"""You are in the main menu.
-    Available functions:
-    {emoji.search} Search (Start search your love)
-    {emoji.fax} Chat (Chatting with randoms)
-    {emoji.settings} Settings (Edit your data)"""
+        (f"You are in the main menu.\n"
+         f"Available functions:\n"
+         f"{emoji.search} Search (Start search your love)\n"
+         f"{emoji.fax} Chat (Chatting with randoms)\n"
+         f"{emoji.settings} Settings (Edit your data)")
     form_heading = \
-        f"""| Your account |"""
+        f"| Your account |"
     main_menu_preparing_suggestions = \
-        f"""Preparing suggestions for you..."""
+        f"Preparing suggestions for you..."
     main_menu_found = \
-        f"""Ready! Found users count:"""
+        f"Ready! Found users count:"
     main_menu_welcome_chat = \
-        f"""Welcome to chat!
-    Here you can find conservations.
-    Press {emoji.search} to find chat!"""
+        (f"Welcome to chat!\n"
+         f"Here you can find conservations.\n"
+         f"Press {emoji.search} to find chat!")
     searching_friend_exit = \
-        f"""That's all for this session!
-    Welcome back to main menu!"""
+        (f"That's all for this session!\n"
+         f"Welcome back to main menu!")
     searching_friend_look_next = \
-        f"""Oh, look what a cool person:"""
+        f"Oh, look what a cool person:"
     searching_friend_guide = \
-        f"""You are in searching friends section!
-    Available functions:
-    {emoji.like} Like (Like person)
-    {emoji.dislike} Next (Get next)
-    {emoji.report} Report (Send a complaint)
-    {emoji.back} Back (Back to the main menu)"""
+        (f"You are in searching friends section!\n"
+         f"Available functions:\n"
+         f"{emoji.like} Like (Like person)\n"
+         f"{emoji.dislike} Next (Get next)\n"
+         f"{emoji.report} Report (Send a complaint)\n"
+         f"{emoji.back} Back (Back to the main menu)")
     searching_friend_match_1 = \
-        f"""Congratulations!
-    This is the match.
-    Here is the link to VK page:"""
+        (f"Congratulations!\n"
+         f"This is the match.\n"
+         f"Here is the link to VK page:")
     searching_friend_match_2 = \
-        f"""Congratulations!
-    You have the match:"""
+        (f"Congratulations!\n"
+         f"You have the match:")
     searching_friend_match_motivation_1 = \
-        f"""Good online time spending!"""
+        f"Good online time spending!"
     searching_friend_match_motivation_2 = \
-        f"""Don't be shy!"""
+        f"Don't be shy!"
     report_was_sent = \
-        f"""Thanks! Report was sent to admin!"""
+        f"Thanks! Report was sent to admin!"
     chat_queue = \
-        f"""You are in queue...
-    Please, wait..."""
+        (f"You are in queue...\n"
+         f"Please, wait...")
     chat_found = \
-        f"""Chat has been found:"""
+        f"Chat has been found:"
     chat_talk = \
-        f"""Talk!"""
+        f"Talk!"
     chat_exit = \
-        f"""OK, goodbye!
-    See ya later..."""
+        (f"OK, goodbye!\n"
+         f"See ya later...")
     chat_guide = \
-        f"""You are in the chat section!
-    Available functions:
-    {emoji.search} Search (Search conservation)
-    {emoji.report} Report (Report user)
-    {emoji.back} Back (Back to main menu)"""
+        (f"You are in the chat section!\n"
+         f"Available functions:\n"
+         f"{emoji.search} Search (Search conservation)\n"
+         f"{emoji.report} Report (Report user)\n"
+         f"{emoji.back} Back (Back to main menu)")
     chatting_exit = \
-        f"""Conversation ended.
-    It was nice... {emoji.sad}"""
+        (f"Conversation ended.\n"
+         f"It was nice... {emoji.sad}")
     settings_guide = \
-        f"""You are in settings menu.
-    Available commands:
-    {emoji.smile} Name (Enter your name)
-    {emoji.glasses} Age (Enter your age)
-    {emoji.talk} About (Write about you)
-    {emoji.camera} Photo (Set avatar photo)
-    {emoji.save} Save (Save changes)"""
+        (f"You are in settings menu.\n"
+         f"Available commands:\n"
+         f"{emoji.smile} Name (Enter your name)\n"
+         f"{emoji.glasses} Age (Enter your age)\n"
+         f"{emoji.talk} About (Write about you)\n"
+         f"{emoji.camera} Photo (Set avatar photo)\n"
+         f"{emoji.save} Save (Save changes)")
     name = "Name"
     age = "Age"
     about = "Bio"
@@ -215,38 +210,37 @@ elif config.LANGUAGE == lang.eng:
     save = "Save"
     cancel = "Cancel"
     settings_name = \
-        f"""What is your name?"""
+        f"What is your name?"
     settings_age = \
-        f"""How old are you?
-    Just don't cheat, please..."""
+        (f"How old are you?\n"
+         f"Just don't cheat, please...")
     settings_about = \
-        f"""Tell me about yourself!
-    I'm really interested in it..."""
+        (f"Tell me about yourself!\n"
+         f"I'm really interested in it...")
     settings_interests = \
-        f"""Choice interests tags for your profile
-    It will improve your suggestions!
-    Just type numbers of tags:
-        """
+        (f"Choice interests tags for your profile\n"
+         f"It will improve your suggestions!\n"
+         f"Just type numbers of tags:\n\n")
     settings_photo = \
-        f"""Everyone want to see you!
-    I'm sure you're beauty.
-    Send photo pls!!"""
+        (f"Everyone want to see you!\n"
+         f"I'm sure you're beauty.\n"
+         f"Send photo pls!!")
     accepted = "Accepted!"
     settings_fill_all = \
-        f"""You must fill all fields!"""
+        f"You must fill all fields!"
     settings_return = \
-        f"""Return you back."""
+        f"Return you back."
     settings_invalid_input = \
-        f"""Invalid Input!"""
+        f"Invalid Input!"
     settings_ok_name = \
-        f"""Nice to meet you!"""
+        f"Nice to meet you!"
     settings_ok_age = \
-        f"""Oh, nice! It's time to find love!
-    In yours age it's necessary!"""
+        (f"Oh, nice! It's time to find love!\n"
+         f"In yours age it's necessary!")
     settings_ok_about = \
-        f"""Ohh, yeah, dude, I knew, that you are cool person!"""
+        f"Ohh, yeah, dude, I knew, that you are cool person!"
     settings_ok_interests = \
-        f"""Interests accepted!"""
+        f"Interests accepted!"
     settings_ok_photo = \
-        f"""You... so cute... oh, sorry, 
-    I have to go... {emoji.shyhands}"""
+        (f"You... so cute... oh, sorry, \n"
+         f"I have to go... {emoji.shyhands}")
